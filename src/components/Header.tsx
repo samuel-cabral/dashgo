9dc92834604f1b21350700b64852fc1f46016fe9
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-03-18 14:46:18 -0300

    Ends header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
 
-import { RiSearchLine } from 'react-icons/ri';
+import {
+  RiUserAddLine,
+  RiNotificationLine,
+  RiSearchLine,
+} from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -44,6 +48,36 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Samuel Cabral</Text>
+            <Text color="gray.300" fontSize="small">
+              samuelcabral.mail@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Samuel Cabral"
+            src="https://github.com/samuel-cabral.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
