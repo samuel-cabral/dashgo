7eefe0b5248176790e6dab1726586b27a3cd21ab
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-03-18 08:48:31 -0300

    Starts dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,40 @@
-export default function Home() {
+import { Button, Flex, Stack, Text } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      flexDirection="column"
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Text fontSize="6xl" fontWeight="bold" letterSpacing="tight">
+        dashgo
+        <Text as="span" fontSize="6xl" fontWeight="bold" color="pink.600">
+          .
+        </Text>
+      </Text>
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        mt="4"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
