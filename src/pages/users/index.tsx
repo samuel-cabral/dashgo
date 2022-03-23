3821c03e1b842c0cb30cecdf5dda36f85238422f
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-03-22 23:31:30 -0300

    Apply responsive rules to user list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,6 +12,7 @@ import {
   Th,
   Thead,
   Tr,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
@@ -20,6 +21,11 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -47,89 +53,98 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={['4', '4', '6']} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8">Ações</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
+                {isWideVersion && <Th width="8">Ações</Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Samuel Cabral</Text>
-                    <Text fontSize="sm" color="gray.300">
+                    <Text fontSize={['xs', 'sm']} color="gray.300">
                       samuelcabral.mail@gmail.com
                     </Text>
                   </Box>
                 </Td>
-                <Td>04 de abril, 2021</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>04 de abril, 2021</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      gap="1"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                    >
+                      <Icon as={RiPencilLine} fontSize="16" />
+                      Editar
+                    </Button>
+                  </Td>
+                )}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Samuel Cabral</Text>
-                    <Text fontSize="sm" color="gray.300">
+                    <Text fontSize={['xs', 'sm']} color="gray.300">
                       samuelcabral.mail@gmail.com
                     </Text>
                   </Box>
                 </Td>
-                <Td>04 de abril, 2021</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>04 de abril, 2021</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      gap="1"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                    >
+                      <Icon as={RiPencilLine} fontSize="16" />
+                      Editar
+                    </Button>
+                  </Td>
+                )}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Samuel Cabral</Text>
-                    <Text fontSize="sm" color="gray.300">
+                    <Text fontSize={['xs', 'sm']} color="gray.300">
                       samuelcabral.mail@gmail.com
                     </Text>
                   </Box>
                 </Td>
-                <Td>04 de abril, 2021</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>04 de abril, 2021</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      gap="1"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                    >
+                      <Icon as={RiPencilLine} fontSize="16" />
+                      Editar
+                    </Button>
+                  </Td>
+                )}
               </Tr>
             </Tbody>
           </Table>
