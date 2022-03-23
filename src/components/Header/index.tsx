c0b806dc5016d11ff45ad1a0cefcc52647b2de0c
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-03-22 22:40:41 -0300

    Apply responsive rules to sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
 
+import { RiMenuLine } from 'react-icons/ri';
 import { NotificationsNav } from './NotificationsNav';
 import { Logo } from './Logo';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
   return (
     <Flex
       as="header"
@@ -21,6 +26,17 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          display="flex"
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
       <Logo />
 
       {isWideVersion && <SearchBox />}
